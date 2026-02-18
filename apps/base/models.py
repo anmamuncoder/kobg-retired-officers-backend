92a3b799e3cf1fe9c4962001b090e0fc32e31c5f
anmamuncoder <anmamuncoder@gmail.com>
2026-02-18 18:25:08 +0600

    deploy(cpanel) : fix some issue that requried in cpanel and update some models

--- apps/base/models.py
+++ apps/base/models.py
@@ -4,9 +4,9 @@ import uuid
 # Create your models here.
 
 class BaseModel(models.Model):
-    id = models.CharField(primary_key=True,default=uuid.uuid4,editable=False)
-    created_at = models.DateTimeField(auto_now_add=True,editable=False)
-    updated_at = models.DateTimeField(auto_now=True,editable=False)
+    id = models.CharField(primary_key=True,default=uuid.uuid4,max_length=36,editable=False)
+    created_at = models.DateTimeField(auto_now_add=True,editable=False,null=True)
+    updated_at = models.DateTimeField(auto_now=True,editable=False,null=True)
 
     class Meta:
         abstract = True
--- core/settings.py
+++ core/settings.py
@@ -72,6 +72,7 @@ INSTALLED_LIBRARY = [
 
 INSTALLED_APPS = (
     [
+        'corsheaders',
         'django.contrib.admin',
         'django.contrib.auth',
         'django.contrib.contenttypes',
